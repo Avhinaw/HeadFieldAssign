92b5d3d918c4889b2cafa45a0933075852ae9e83
avhinaw <avishkarshrivastav@gmail.com>
2025-03-16 16:59:35 +0530

    optimized responsiveness

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ interface ButtonProp{
 }
 const Button: React.FC<ButtonProp> = ({text}) => {
   return (
-    <div className="px-4 lg:px-0">
+    <div className="lg:px-0">
         <a
             href="/"
             className="inline-block text-yellow-500 border font-bold py-1 px-5 rounded-4xl hover:border-yellow-700 hover:text-white hover:bg-yellow-700"
